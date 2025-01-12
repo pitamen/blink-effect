541f466ad4ac8d5fdecd8812a6d0448ba3b584dc
Pitambar Koirala <pitambar.koirala@dishhome.com.np>
2025-01-12 13:48:03 +0545

    Initial Commit

---
+++ BlinkEffect/src/main/java/com/dishhome/blinkeffect/BlinkEffect.kt
@@ -0,0 +1,7 @@
+package com.dishhome.blinkeffect
+
+object BlinkEffect {
+    fun blink(myText: String): String {
+        return myText.uppercase()
+    }
+}
---
+++ app/src/main/java/com/dishhome/blinkeffectexample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.dishhome.blinkeffectexample
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dishhome.blinkeffect.BlinkEffect
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val mainTextView = findViewById<TextView>(R.id.mainText)
+        mainTextView.text = BlinkEffect.blink(mainTextView.text.toString())
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
